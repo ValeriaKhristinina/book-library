006d308b7215fd79bfcba74126c16d0225adef6c
Valeria Khristinina <valeria.khristinina@gmail.com>
2022-01-11 13:16:19 +0100

    created simple footer and added to main page

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import './footer.css'
+
+function Footer(): JSX.Element {
+  return (
+    <section className="footer container">
+      <section className="copyright"><span>Created by</span> <a href="https://github.com/ValeriaKhristinina" target="blank">Lera Khristinina</a></section>
+    </section >
+  )
+}
+
+export default Footer
--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -1,11 +1,13 @@
 import Header from '../header/header';
 import Content from '../content/content';
+import Footer from '../footer/footer'
 
 function MainPage() {
   return (
     <>
       <Header />
       <Content />
+      <Footer />
     </>
   )
 }
