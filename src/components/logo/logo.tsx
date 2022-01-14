1545841984f66d0ea6bdea61666bbb7543c20f66
Valeria Khristinina <valeria.khristinina@gmail.com>
2022-01-14 13:13:21 +0100

    created logo component

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,11 @@
+import './logo.css';
+
+function Logo(): JSX.Element {
+  return (
+    <div className="logo">
+      <img src="https://img.icons8.com/doodle/96/000000/books.png" alt="logo" />
+    </div>
+  )
+}
+
+export default Logo;
